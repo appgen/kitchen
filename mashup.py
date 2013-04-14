a9fb0be9c475d1170e40bbc575915d442c1ca96c
Thomas Levine <occurrence@thomaslevine.com>
2013-04-14 03:09:42 -0400

    tags

--- mashup.py
+++ mashup.py
@@ -17,13 +17,22 @@ def group_columns(views):
             column_types[key].add(view['id'])
     return column_types
 
+def group_tags(views):
+    tags = {}
+    for view in views:
+        for tag in view.get('tags', []):
+            if tag not in tags:
+                tags[tag] = set()
+            tags[tag].add(view['id'])
+    return tags
 
 if __name__ == '__main__':
     import os
     import json
     view_dir = os.path.join('pantry', 'socrata', 'views')
     view_ids = os.listdir(view_dir)
-    views = (json.load(open(os.path.join(view_dir, view_id))) for view_id in view_ids)
+    views = [json.load(open(os.path.join(view_dir, view_id))) for view_id in view_ids]
     for k, v in group_columns(views).items():
-        if len(v) > 1:
-            print k, v
+        if len(v) > 30:
+            print k
+    print group_tags(views)
