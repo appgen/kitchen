5c8099a1a80db63cbc6a82bc71d73711454b5be3
Thomas Levine <occurrence@thomaslevine.com>
2013-04-14 10:05:25 -0400

    url tree

--- mashup.py
+++ mashup.py
@@ -2,6 +2,37 @@
 '''
 Mash up multiple datasets.
 '''
+from copy import deepcopy
+from urlparse import urlparse
+
+empty_tree = [{}, set()]
+def _build_tree(tree, path, viewid):
+    '''
+    Build a tree by which we can search urls. Each subtree is a tuple of a dict
+    of urls and a set of 4x4 Socrata identifiers.
+    '''
+    # If we have stuff in the path that are not in the tree, add an empty branch.
+    if path[0] not in tree[0]:
+        tree[0][path[0]] = deepcopy(empty_tree)
+
+    # Recurse
+    if len(path) == 1:
+        # We have no more items in the path, so we're done.
+        tree[0][path[0]][1].add(viewid)
+    else:
+        # Add the next url part
+        tree[0][path[0]] = _build_tree(tree[0][path[0]], path[1:], viewid)
+    return tree
+
+
+def tree_affiliation_links(views):
+    link_tree = deepcopy(empty_tree)
+    for view in views:
+        if 'attributionLink' in view:
+            url = urlparse(view['attributionLink'])
+            path = [url.netloc] + filter(None, url.path.split('/') + [url.query])
+            link_tree = _build_tree(link_tree, path, view['id'])
+    return link_tree
 
 def group_tags(views):
     return group(views, lambda view: view.get('tags', []))
