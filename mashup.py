0ee8fbb73dcffed0eea06760e38128cf94b84c07
Thomas Levine <occurrence@thomaslevine.com>
2013-04-17 23:12:21 -0400

    revert

--- mashup.py
+++ mashup.py
@@ -4,23 +4,92 @@ Mash up multiple datasets.
 '''
 from urlparse import urlparse
 from copy import deepcopy
-from lxml.etree import Element, XML
+
+class Attributiontree:
+    def __init__(self, tree = [{}, set()]):
+        self._tree = tree
+
+    # Construction
+    # ---------------------------------
+
+    # The empty tree
+    empty = [{}, set()]
+
+    def build(self, path, viewid):
+        self._tree = Attributiontree._build_tree(self._tree, path, viewid)
+
+    @staticmethod
+    def _build_tree(tree, path, viewid):
+        '''
+        Build a tree by which we can search urls. Each subtree is a tuple of a dict
+        of urls and a set of 4x4 Socrata identifiers.
+        '''
+        # If we have stuff in the path that are not in the tree, add an empty branch.
+        if path[0] not in tree[0]:
+            tree[0][path[0]] = Attributiontree.empty
+        subtree = deepcopy(tree[0][path[0]])
+
+        # Recurse
+        if len(path) == 1:
+            # We have no more items in the path, so we're done.
+            subtree[1].add(viewid)
+        else:
+            # Add the next url part
+            subtree = Attributiontree._build_tree(subtree, path[1:], viewid)
+
+        tree[0][path[0]] = subtree
+        return tree
+
+    # Querying
+    # ---------------------------------
+    def __repr__(self):
+        def shorten(param):
+            if len(param) > 2:
+                param = param[:2] + ['...']
+            return ', '.join(param)
+        params = map(shorten, [self.children().keys(), list(self.current_viewids())])
+        return 'Children: %s  |  Viewids: %s' % tuple(params)
+
+    def children(self):
+        return {k:Attributiontree(v) for k,v in self._tree[0].items()}
+
+    def descendant(self, path):
+        'Find things with similar urls.'
+        tree = deepcopy(self)
+        for subdir in filter(None, path.split('/')):
+            tree = tree.children()[subdir]
+        return tree
+
+    def child_paths(self):
+        return set(self._tree[0].keys())
+
+    def descendant_paths(self):
+        todo = [([], self)]
+        while len(todo) > 0:
+            path, tree = todo.pop()
+            for filename, child in tree.children().items():
+                if len(child.current_viewids()) > 0:
+                    yield '/'.join(path + [filename])
+            todo.extend([(path + [subdir], v) for subdir, v in tree.children().items()])
+
+    def current_viewids(self):
+        return self._tree[1]
+
+    def descendant_viewids(self):
+        todo = [self]
+        while len(todo) > 0:
+            current = todo.pop()
+            for viewid in current.current_viewids():
+                yield viewid
+            todo.extend(current.children().values())
 
 def attribution_links(views):
-    link_tree = Element('a')
+    link_tree = Attributiontree()
     for view in views:
         if 'attributionLink' in view:
             url = urlparse(view['attributionLink'])
             path = [url.netloc] + filter(None, url.path.split('/') + [url.query])
-
-            xpath = ''
-            for part in path:
-                xpath += '/' + part
-                if link_tree.xpath(xpath) == []:
-                    link_tree.append(Element('a'))
-
-            link_tree.xpath(xpath)[0].append(XML('<viewid>%s</viewid>' % view['attributionLink']))
-
+            link_tree.build(path, view['id'])
     return link_tree
 
 def group_tags(views):
@@ -59,3 +128,5 @@ if __name__ == '__main__':
             print k
 
     t = attribution_links(views)
+    print set(t.descendant('schools.nyc.gov/NR/rdonlyres/66E8CC55-51E7-4DE5-8C5C-08C588701A1E').descendant_viewids())
+    print set(t.descendant('www.nyc.gov/html/dcp/html').descendant_paths())
