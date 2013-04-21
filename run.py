031dc06daa782a99b5dbdbc8b4189db1156c90d7
Thomas Levine <occurrence@thomaslevine.com>
2013-04-21 14:04:54 -0400

    copy-writing

--- run.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python2
-from dumptruck import DumpTruck
-
-from collabfinder import answers
--- write_copy.py
+++ write_copy.py
@@ -2,4 +2,8 @@
 '''
 Write copy about the app. References:
 * http://www.slideshare.net/ogrisel/nltk-scikit-learnpyconfr2010ogrisel
+* https://gist.github.com/322906/90dea659c04570757cccf0ce1e6d26c9d06f9283
+* http://stackoverflow.com/questions/5708352/named-entity-recognition-for-nltk-in-python-identifying-the-ne
+* http://nltk.googlecode.com/svn/trunk/doc/book/ch07.html
+* https://github.com/tlevine/crawl-collabfinder
 '''
