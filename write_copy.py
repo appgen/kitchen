c31d8884edf6c0f838b0817d4b87b868f4a7100a
Thomas Levine <occurrence@thomaslevine.com>
2013-04-23 08:47:32 -0400

    remove junk write diamonds

--- write_copy.py
+++ write_copy.py
@@ -6,21 +6,26 @@ Write copy about the app. References:
 * http://stackoverflow.com/questions/5708352/named-entity-recognition-for-nltk-in-python-identifying-the-ne
 * http://nltk.googlecode.com/svn/trunk/doc/book/ch07.html
 * https://github.com/tlevine/crawl-collabfinder
+
+
+# Ideas
+chunked = nltk.batch_ne_chunk(tagged, binary = True)
+
 '''
 
 import re
 from string import ascii_letters
 
 import nltk
-from nltk.model import NgramModel
+import nltk.model
 
 import collabfinder
 
 LEFT = '(['
-def detokenize(tokens):
+def dewordize(words):
+    'Turn a list of words and punctuation into a string.'
     text = ''
-    for token in tokens:
-        word = token[0] # token[1] is pos
+    for word in words:
         if word[0] in (ascii_letters + LEFT):
             # Add a space
             text += ' '
@@ -31,39 +36,35 @@ def detokenize(tokens):
         text += word
     return re.sub(r'\.[^.]*$', '.', text[1:])
 
-def train_subdescription(subdescriptions):
+def annotate_first_sentence(subdescriptions):
+    '''
+    Build an n-gram model of the first sentence.
+    :param subdescription: a list of "what", "why" or "need" string
+    :type subdescription: list(unicode)
+    '''
+    return (nltk.pos_tag(nltk.word_tokenize(nltk.sent_tokenize(subdescription)[0])) for subdescription in subdescriptions)
+
+def annotate_full_subdescription(subdescriptions):
     '''
     :param subdescriptions: a list of training texts
-    :type subdescriptions: list(list(str))
+    :type subdescriptions: list(unicode)
     '''
-    train = [nltk.pos_tag(nltk.wordpunct_tokenize(d)) for d in subdescriptions]
-    lm = NgramModel(3, train, estimator = (lambda fdist, bins: nltk.LidstoneProbDist(fdist, 0.2)))
-    return lm
+    return (nltk.pos_tag(nltk.wordpunct_tokenize(subdescription)) for subdescription in subdescriptions)
 
-def something():
-    sentences = []
-    for a in collabfinder.answers():
-        if a['description']:
-            sentences.extend(nltk.sent_tokenize(a['description']['what']))
+def ngram_model(annotation):
+    'Build a model given a the pos-tagged annotation.'
+    m = nltk.model.NgramModel(3, train, estimator = (lambda fdist, bins: nltk.LidstoneProbDist(fdist, 0.2)))
+    return m
 
-    tagged = (nltk.pos_tag(nltk.word_tokenize(sentence)) for sentence in sentences)
-    chunked = nltk.batch_ne_chunk(tagged, binary = True)
-    for tree in chunked:
-        print '---'
-        print unicode(tree)
-        print extract_entity_names(tree)
-
-from nltk.model.api import ModelI
-class CollabfinderModel(ModelI):
-    'http://nltk.org/_modules/nltk/model/ngram.html#NgramModel'
-    pass
-
-if __name__ == '__main__':
+def get_subdescriptions():
     import os
     import json
 
     descriptions = filter(None, [project['description'] for project in collabfinder.answers()])
-    models = {}
+    subdescriptions = {}
     for key in ['what', 'why', 'need']:
-        subdescriptions = [d[key] for d in descriptions]
-        models[key] = train_subdescription(subdescriptions)
+        subdescriptions[key] = [d[key].strip() for d in descriptions]
+    return subdescriptions
+
+if __name__ == '__main__':
+    models[key] = train_subdescription(subdescriptions)
