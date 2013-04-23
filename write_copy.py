7ff092882953a83943c4aa1b4f351cb0c9fbfb18
Thomas Levine <occurrence@thomaslevine.com>
2013-04-23 08:30:20 -0400

    stuff

--- write_copy.py
+++ write_copy.py
@@ -11,14 +11,8 @@ Write copy about the app. References:
 import re
 from string import ascii_letters
 
-from random import normalvariate
-
-from numpy import array
-
-from nltk.tokenize import wordpunct_tokenize #, word_tokenize, sent_tokenize
-from nltk.probability import LidstoneProbDist #LaplaceProbDist
-from nltk import NgramModel
 import nltk
+from nltk.model import NgramModel
 
 import collabfinder
 
@@ -26,38 +20,25 @@ LEFT = '(['
 def detokenize(tokens):
     text = ''
     for token in tokens:
-        if token[0] in (ascii_letters + LEFT):
+        word = token[0] # token[1] is pos
+        if word[0] in (ascii_letters + LEFT):
             # Add a space
             text += ' '
-        elif token[0] in LEFT:
+        elif word[0] in LEFT:
             # Remove a space
             text = text[:-1]
 
-        text += token
+        text += word
     return re.sub(r'\.[^.]*$', '.', text[1:])
-
-MINIMUM_DESCRIPTION_LENGTH = 120
-def description_length_func(subdescriptions):
-    'Create a function that chooses the length of a description.'
-    a = array([len(d) for d in subdescriptions])
-    mean = a.mean()
-    std = a.std()
-    def description_length():
-        l = normalvariate(mean, std)
-        return int(max(MINIMUM_DESCRIPTION_LENGTH, l))
-    return description_length
 
 def train_subdescription(subdescriptions):
     '''
     :param subdescriptions: a list of training texts
     :type subdescriptions: list(list(str))
     '''
-    train = [wordpunct_tokenize(d) for d in subdescriptions]
-    lm = NgramModel(3, train, estimator = (lambda fdist, bins: LidstoneProbDist(fdist, 0.2)))
-
-    length = description_length_func(subdescriptions)
-
-    return lm, length
+    train = [nltk.pos_tag(nltk.wordpunct_tokenize(d)) for d in subdescriptions]
+    lm = NgramModel(3, train, estimator = (lambda fdist, bins: nltk.LidstoneProbDist(fdist, 0.2)))
+    return lm
 
 def something():
     sentences = []
@@ -72,16 +53,6 @@ def something():
         print unicode(tree)
         print extract_entity_names(tree)
 
-def extract_entity_names(tree):
-    entity_names = []
-    if hasattr(tree, 'node') and tree.node:
-        if tree.node == 'NE':
-            entity_names.append(' '.join([child[0] for child in tree]))
-        else:
-            for child in tree:
-                entity_names.extend(extract_entity_names(child))
-    return entity_names
-
 from nltk.model.api import ModelI
 class CollabfinderModel(ModelI):
     'http://nltk.org/_modules/nltk/model/ngram.html#NgramModel'
@@ -92,8 +63,7 @@ if __name__ == '__main__':
     import json
 
     descriptions = filter(None, [project['description'] for project in collabfinder.answers()])
-    generators={}
+    models = {}
     for key in ['what', 'why', 'need']:
         subdescriptions = [d[key] for d in descriptions]
-        lm, length = train_subdescription(subdescriptions)
-        generators[key] = lambda about: detokenize(lm.generate(length(), wordpunct_tokenize(about)))
+        models[key] = train_subdescription(subdescriptions)
