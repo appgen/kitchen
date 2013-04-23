c7ad347c18421dfcc16efb4d3cd53df3fa480b31
Thomas Levine <occurrence@thomaslevine.com>
2013-04-23 14:31:26 -0400

    read csv

--- join.py
+++ join.py
@@ -1,13 +1,21 @@
 import os
 import json
+
+import pandas
+
 from group import group_columns
 
-def views():
-    view_dir = os.path.join('pantry', 'socrata', 'views')
-    view_ids = os.listdir(view_dir)
-    return [json.load(open(os.path.join(view_dir, view_id))) for view_id in view_ids]
+SOCRATA = os.path.join('pantry', 'socrata')
+VIEWS = os.path.join(SOCRATA, 'views')
+
+def rows(viewid):
+    return pandas.io.parsers.read_csv(os.path.join(SOCRATA, 'rows', viewid))
+
+def viewids():
+    view_ids = os.listdir(VIEWS)
+    return [json.load(open(os.path.join(VIEWS, view_id))) for view_id in view_ids]
 
 def columns():
     pass # for k, v in group_columns(views()).items()
 
-example = group_columns(views())[(u'text', u'neighborhood')]
+example = group_columns(viewids())[(u'text', u'neighborhood')]
