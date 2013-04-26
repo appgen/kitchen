fd426f0b95ea3311938c9076283aed0f41ad5303
Thomas Levine <occurrence@thomaslevine.com>
2013-04-26 10:04:14 -0400

    sample graph

--- explore.py
+++ explore.py
@@ -1,6 +1,7 @@
 from itertools import combinations
 
 from Levenshtein import distance
+# import matplotlib.pyplot as plt
 
 import networkx
 g = networkx.Graph()
@@ -10,8 +11,12 @@ c = columns()
 
 # Nodes
 for k,v in c.items():
-    g.add_node(k, { 'datasets': v })
+    g.add_node('\n'.join(k), { 'name': k[1], 'type': k[0], 'datasets': v })
 
 # Edges
-for low, high in combinations(c.keys(), 2):
-    g.add_edge(low, high, { 'distance': distance(low, high) })
+from random import sample
+for low, high in combinations(sample(c.keys(), 100), 2):
+    # If they're of similar length,
+    if (len(low[1]) - len(high[1])) / (len(low[1]) + len(high[1])) < (1/3):
+        # add the edge.
+        g.add_edge('\n'.join(low), '\n'.join(high), { 'distance': distance(low[1], high[1]) })
