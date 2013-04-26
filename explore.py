bcb0df1a857303160e4a1e769a8d39ebcfea072c
Thomas Levine <occurrence@thomaslevine.com>
2013-04-26 13:01:15 -0400

    reduce data

--- explore.py
+++ explore.py
@@ -7,16 +7,13 @@ import networkx
 g = networkx.Graph()
 
 from socrata import columns
-c = columns()
+c = {k:v for k,v in columns() if len(v) > 10}
 
 # Nodes
 for k,v in c.items():
-    g.add_node('\n'.join(k), { 'name': k[1], 'type': k[0], 'datasets': v })
+    g.add_node(k, { 'name': k[1], 'type': k[0], 'datasets': v })
 
 # Edges
 from random import sample
-for low, high in combinations(sample(c.keys(), 100), 2):
-    # If they're of similar length,
-    if (len(low[1]) - len(high[1])) / (len(low[1]) + len(high[1])) < (1/3):
-        # add the edge.
-        g.add_edge('\n'.join(low), '\n'.join(high), { 'distance': distance(low[1], high[1]) })
+for low, high in combinations(c.keys(), 2):
+    g.add_edge(low, high, { 'distance': distance(low[1], high[1]) })
