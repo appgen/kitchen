69711191a03b14924b7ae57b00828fcbee0e0f56
Thomas Levine <occurrence@thomaslevine.com>
2013-04-30 11:59:40 -0400

    cdf test

--- test_write_copy.py
+++ test_write_copy.py
@@ -1,4 +1,6 @@
 import nose.tools as n
+from collections import Counter
+
 import write_copy
 
 def test_parse():
@@ -13,3 +15,29 @@ def test_parse():
     n.assert_dict_equal(observed['frequencies'], expected['frequencies'])
     n.assert_list_equal(observed['sequence'], expected['sequence'])
     n.assert_list_equal(observed.keys(), expected.keys())
+
+def test_cdf():
+    '''
+    cdf should build a cumulative distribution function of bigram latter halves
+    based on a generator of collections.Counter.
+    '''
+    def example():
+        yield Counter('abe')
+        yield Counter('aabb')
+        yield Counter('acb')
+
+    observed = write_copy.cdf(example(), weight = 1)
+    for k,v in observed.items():
+        observed[k] = set(v)
+
+    expected = {
+        '^': {(1.00, 'a')},
+        'a': {(0.25, 'b'),
+              (0.75, 'a'),
+              (1.00, 'c')},
+        'b': {(0.25, 'e'),
+              (0.50, 'b'),
+              (1.00, '$')},
+        'c': {(1.00, 'b')}
+        'e': {(1.00, '$')}
+        }
