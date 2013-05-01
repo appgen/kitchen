21ec69f78bbeae3f30ee72d709a3b4df9a9d70ee
Thomas Levine <occurrence@thomaslevine.com>
2013-05-01 18:42:11 -0400

    move more junk

--- socrata.py
+++ socrata.py
@@ -3,24 +3,39 @@ import json
 
 import pandas
 
-from socrata_group import distinct_columns
-
 SOCRATA = os.path.join('pantry', 'socrata')
 VIEWS = os.path.join(SOCRATA, 'views')
 
-def rows(viewid):
+def viewdict():
+    'A hash from viewid to the metadata'
+    return {view_id: json.load(open(os.path.join(VIEWS, view_id))) for view_id in os.listdir(VIEWS)}
+
+def columndict():
+    'A hash from column (type,name) to viewids'
+    views = viewdict().values()
+    everything = _group(views, lambda view: [(col['dataTypeName'], col['fieldName']) for col in view.get('columns', [])]).items()
+    return {k:v for k,v in everything.items() if k != (u'geospatial', u'shape')}
+
+def _rows(viewid):
+    'Get the rows for a viewid'
     return pandas.io.parsers.read_csv(os.path.join(SOCRATA, 'rows', viewid))
 
-def viewids():
-    return os.listdir(VIEWS)
+def _group(views, func):
+    '''
+    Produce a dictionary mapping column types to all of the datasets containing
+    whatever property.
+    '''
+    items = {}
+    for view in views:
+        for item in func(view):
+            if item not in items:
+                items[item] = set()
+            items[item].add(view['id'])
+    return items
 
-def viewdict():
-    return {view_id: json.load(open(os.path.join(VIEWS, view_id))) for view_id in viewids()}
-
-def join(column_name, viewids):
+def join(column_name, dfs):
+    'Join a bunch of dataframes on a column name'
     # Load
-    dfs = map(row, viewids)
-
     for df in dfs:
         # Lowercase names
         df.columns = [name.lower() for name in df.columns]
@@ -37,7 +52,5 @@ def join(column_name, viewids):
     return left
 
 if __name__ == '__main__':
-    # example = join(u'building_address', distinct_columns(viewdict().values())[(u'text', u'building_address')])
     v = viewdict()
-    c = distinct_columns(v.values()))
-    join(u'building_address',
+    c = columndict()
