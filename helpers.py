c2e2720b1e64f6e17c031825c5ea46e740038b56
Thomas Levine <occurrence@thomaslevine.com>
2013-05-14 22:23:36 -0400

    cache decorator

---
+++ helpers.py
@@ -0,0 +1,45 @@
+# From hardhat
+import os
+
+# Cache
+import pickle
+
+def save(key, value, cache_dir = '/tmp/appgen'):
+    'Save an object to the cache_dir.'
+    cache_path = os.path.join(cache_dir, key + '.p')
+    cache_file = open(cache_path, 'wb')
+    pickle.dump(value, cache_file)
+    cache_file.close()
+
+def load(key, cache_dir = '/tmp/appgen'):
+    'Load an object from the cache_dir.'
+    cache_path = os.path.join(cache_dir, key + '.p')
+    if os.path.exists(cache_path):
+        cache_file = open(cache_path, 'rb')
+        data = pickle.load(cache_file)
+        cache_file.close()
+    else:
+        raise KeyError(key)
+    return data
+
+def cache(key, cache_dir = '/tmp/appgen'):
+    '''
+    Check if a value is in the cache.
+    Load and cache it from the function if it isn\'t already cached.
+    '''
+    def decorator(func):
+        def wrapper():
+            cache_path = os.path.join(cache_dir, key + '.p')
+
+            if os.path.exists(cache_path):
+                data = load(key, cache_dir = cache_dir)
+            else:
+                # Run the function
+                data = func()
+
+                # Cache
+                save(key, data, cache_dir = cache_dir)
+
+            return data
+        return wrapper
+    return decorator
