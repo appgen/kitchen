96732388346f81d0881b3487f0ea9f193cf8d01b
Thomas Levine <occurrence@thomaslevine.com>
2013-05-14 23:32:44 -0400

    flatten the dictionary. yay.

--- metametadata.py
+++ metametadata.py
@@ -4,36 +4,22 @@ import socrata
 from copy import copy
 from helpers import cache
 
-class NestedDict(dict):
-    @classmethod
-    def nested_keys(nested_dict, prefix = []):
-        for parent_key, parent in nested_dict.items():
-            if hasattr(parent, 'items'):
-                for child_key in NestedDict.nested_keys(parent, prefix = prefix + [parent_key]):
-                    yield child_key
-            else:
-                yield prefix + [parent_key]
+def _nested_dict_iter(nested, sep):
+    for key, value in nested.iteritems():
+        if hasattr(value, 'iteritems'):
+            for inner_key, inner_value in _nested_dict_iter(value):
+                yield key + sep + inner_key, inner_value
+        else:
+            yield key, value
 
-    def nested_get(self, nested_key):
-        return reduce(lambda a,b: a[b], [self] + nested_key)
-
-    def flatten(self, sep = '.'):
-        'Flatten a dictionary, replacing nestings with dots or whatnot.'
-        return {k:self.nested_get(k) for k in NestedDict.nested_keys(self)}
-
-def _flatten_pair(key, value):
-    if hasattr(value, 'items'):
-        return {(key + sep + a):b for a,b in value.items()}
-    else:
-        return {key:value}
-
+def flatten(nested, sep = '.'):
+    'Flatten a dictionary, replacing nested things with dots.'
+    return dict(_nested_dict_iter(nested, sep))
 
 def main():
     viewdict = cache('viewdict', socrata.viewdict)
-    for viewid, metadata in viewdict.items():
-        metadata.update({
-        })
-        yield metadata
+    for viewid, view in viewdict.items():
+        viewdict[viewid][
 
 if __name__ == '__main__':
     import json
