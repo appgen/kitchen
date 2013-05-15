b73b485f1f1f72e18d53041379e9460ad344b1bc
Thomas Levine <occurrence@thomaslevine.com>
2013-05-14 23:57:41 -0400

    Save to SQLite3

--- audit.py
+++ audit.py
@@ -2,12 +2,8 @@
 import re
 
 import socrata
+import write
 from helpers import cache, flatten
-
-uniondict = socrata.uniondict()
-uniondict_broad = socrata.uniondict_broad()
-viewdict = socrata.viewdict()
-# columndict = socrata.columndict(minmatches = 1)
 
 def wide_format(view):
     'Assume that the dataset is in long format, but return true if it seems wide.'
@@ -43,23 +39,38 @@ def subsets(_uniondict):
         title = socrata.combine_titles([viewdict[viewid] for viewid in viewids])
         yield {'title': title, 'schema': schema, 'datasets': datasets}
 
-def subset_statistics():
+def subset_statistics(uniondict_broad):
     n_datasets = len(viewdict)
     n_unionable = sum(map(len, uniondict_broad.values()))
     n_union_result = len(uniondict_broad)
     return n_datasets, n_unionable, n_union_result
 
-if __name__ == '__main__':
-    import json
-    json.dump(list(subsets(uniondict_broad)), open('comestibles/unionable.json', 'w'))
-    # print subset_statistics()
+# if __name__ == '__main__':
+#     import json
+#     json.dump(list(subsets(uniondict_broad)), open('comestibles/unionable.json', 'w'))
 
 if __name__ == '__main__':
+    # Load the base information.
+    # generators = cache('generators', write.build_generators)
     viewdict = cache('viewdict', socrata.viewdict)
-    columndict = cache('columndict', socrata.columndict)
     uniondict = cache('uniondict', socrata.uniondict)
-    generators = cache('generators', write.build_generators)
 
-    viewdict = cache('viewdict', socrata.viewdict)
-    for viewid, view in viewdict.items():
-        viewdict[viewid]['column_matches']
+    # uniondict_broad = socrata.uniondict_broad()
+
+    # Connect to the database.
+    from dumptruck import DumpTruck
+    dt = DumpTruck('/tmp/appgen.db', auto_commit = False)
+    dt.drop('dataset', if_exists = True)
+    dt.create_table(viewdict.values()[0], 'dataset')
+    dt.create_index(['name'], if_not_exists = True, unique = True)
+
+    # Add the column references.
+    columndict = cache('columndict', socrata.columndict)
+    for column, new_viewids in columndict.items():
+        old_viewids = viewdict[viewid].get('column_matches', [])
+        viewdict[viewid]['column_matches'] = list(set(before + viewids))
+
+    # Save to the database.
+    for view in viewdict.values():
+        dt.insert(view, 'dataset')
+    dt.commit()
