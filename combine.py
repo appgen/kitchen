aad2cf8b2c2ae8cfd720279edd867cad899c504c
Thomas Levine <occurrence@thomaslevine.com>
2013-05-26 18:21:17 -0700

    Write a cleaner unioner.

--- combine.py
+++ combine.py
@@ -1,118 +1,28 @@
-#!/usr/bin/env python2
-import os
-import re
-import json
-
 import pandas
+VIEWS = os.path.join('pantry', 'socrata', 'views')
+ROWS = os.path.join('pantry', 'socrata', 'rows')
 
-import socrata
-import write
-from helpers import cache, flatten
+def _union(viewids):
+    data = [pandas.read_csv(os.path.join(ROWS, viewid)) for viewid in viewids]
+    return pandas.concat(data)
 
-def wide_format(view):
-    'Assume that the dataset is in long format, but return true if it seems wide.'
-    years = boroughs = numbers = []
-    for c in view['columns']:
-        name = c['name']
+def _combiner(func):
+    'Create a combiner function'
+    def g(viewids):
+        seed = str(func) + ','.join(viewids)
 
-        year = re.match(r'.*[12][90][0-9][0-9].*', name)
-        borough = re.match(r'.*(bronx|manhattan|queens|brooklyn|staten).*', name, flags = re.IGNORECASE)
-        number = re.match(r'_[0-9]', name)
+        # Save CSV data
+        df = func(viewids)
+        df.to_csv(os.path.join('comestibles', '%d.csv' % seed))
 
-        if year:
-            years.append(year.group())
-        if borough:
-            boroughs.append(year.group())
-        if number:
-            numbers.append(number.group())
+        # Save JSON metadata
+        metadata = {
+            'seed': seed,
+            'sources': [json.load(open(os.path.join(VIEWS, viewid))) for viewid in viewids],
+        }
+        json.dump(metadata, os.path.join('comestibles', '%d.json' % seed))
 
-    for variable in [years, borough, numbers]:
-        if len(variable) > 1:
-            return True
-    return False
+    return g
 
-def subsets(_uniondict):
-    for schema, viewids in _uniondict.items():
-        datasets = [{
-            'id': viewdict[viewid]['id'],
-            'name': viewdict[viewid]['name'],
-            'attribution': viewdict[viewid].get('attribution'),
-            'uploader': viewdict[viewid]['owner']['displayName'],
-           #'nrow': len(socrata._rows(viewid)),
-        } for viewid in viewids]
-        title = socrata.combine_titles([viewdict[viewid] for viewid in viewids])
-        yield {'title': title, 'schema': schema, 'datasets': datasets}
-
-def subset_statistics(uniondict_broad):
-    n_datasets = len(viewdict)
-    n_unionable = sum(map(len, uniondict_broad.values()))
-    n_union_result = len(uniondict_broad)
-    return n_datasets, n_unionable, n_union_result
-
-def build_db(dt):
-    # Load the base information.
-    viewdict = cache('viewdict', socrata.viewdict)
-
-    dt.drop('dataset', if_exists = True)
-    dt.create_table(viewdict.values()[0], 'dataset')
-    dt.create_index(['id'], 'dataset', if_not_exists = True, unique = True)
-
-    # Add the join references (shared columns).
-    columndict = cache('columndict', socrata.columndict)
-    for column, new_viewids in columndict.items():
-        for viewid in new_viewids:
-            old_viewids = set(viewdict[viewid].get('joinable', []))
-            viewdict[viewid]['joinable'] = list(old_viewids.union(new_viewids))
-
-    # Add the union references (shared schema)
-    uniondict = cache('uniondict', socrata.uniondict)
-    for schema, viewids in uniondict.items():
-        for viewid in viewids:
-            viewdict[viewid]['schema'] = list(schema)
-            viewdict[viewid]['unionable'] = list(viewids)
-
-    # uniondict_broad = socrata.uniondict_broad()
-    # generators = cache('generators', write.build_generators)
-
-
-    # Save to the database.
-    for view in viewdict.values():
-        # There's a lot of interesting stuff in these fields,
-        # but they're big or weakly structured.
-        # Remove these because they're big
-        for key in ['columns', 'metadata', 'displayFormat', 'viewFilters', 'query']:
-            if key in view:
-                del(view[key])
-
-        # Flatten
-        dt.insert(flatten(view), 'dataset')
-    dt.commit()
-
-    return dt
-
-def union(dt):
-    'Union the datasets, and write them to CSV and JSON (just ids), each named with a hash of the schema.'
-    schemata = [row['schema'] for row in dt.execute('SELECT DISTINCT "schema" FROM "dataset" WHERE "schema" NOT NULL;')]
-    for schema in schemata:
-        dataset_ids = [dataset['id'] for dataset in dt.execute('SELECT id FROM "dataset" WHERE "schema" = ?', [schema])]
-        data = [pandas.read_csv(os.path.join(socrata.SOCRATA, 'rows', dataset_id)) for dataset_id in dataset_ids]
-        unioned = pandas.concat(data)
-        f = open(os.path.join('comestibles', str(json.dumps(schema).__hash__())) + '.csv', 'w')
-        unioned.to_csv(f)
-        f.close()
-        g = open(os.path.join('comestibles', str(json.dumps(schema).__hash__())) + '.json', 'w')
-        json.dump(dataset_ids, g)
-        g.close()
-        del(data)
-        del(unioned)
-
-def main():
-    # Connect to the database.
-    from dumptruck import DumpTruck
-    dt = DumpTruck('/tmp/appgen.db', auto_commit = False)
-    if 'dataset' not in dt.tables():
-        dt = build_db()
-    union(dt)
-
-if __name__ == '__main__':
-    main()
+# Exposed functions
+union = _combiner(_union)
