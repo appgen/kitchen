c288dbe42ef3ebe27483cad77976ee97e43139a8
Thomas Levine <occurrence@thomaslevine.com>
2013-05-26 19:05:59 -0700

    memory limit

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python2
 import socrata, write
 from helpers import cache
 from combine import union
 
+# Load source data
 # viewdict = cache('viewdict', socrata.viewdict)
 # columndict = cache('columndict', socrata.columndict)
 
@@ -11,6 +13,15 @@ uniondict = cache('uniondict', socrata.uniondict)
 print('Building copy generators')
 generators = write.build_generators()
 
+# Limit memory usage
+# http://stackoverflow.com/questions/2308091/how-to-limit-python-heap-size
+MEM_GB = 10
+import resource
+rsrc = resource.RLIMIT_DATA
+soft, hard = resource.getrlimit(rsrc)
+resource.setrlimit(rsrc, (MEM_GB * (2 ** 30), hard))
+
+# Generate the files.
 import json
 for schema, viewids in uniondict.items():
     print('Appifying schema ' + json.dumps(schema))
