0e403c5553a3ae7db8084849fb144e2c06c94120
Thomas Levine <occurrence@thomaslevine.com>
2013-06-04 17:06:28 -0400

    fix the s3 upload script

--- upload.sh
+++ upload.sh
@@ -2,12 +2,16 @@
 # Upload comestibles
 set -e
 
-for geojson in comestibles/*.geojson; do
-  seed=$(basename $geojson .geojson)
-  for extension in json csv geojson; do
-    if test -f "./$seed.$extension" && ! test -f "./$seed.$extension.gz"; then
-      gzip "./$seed.$extension"
-    fi
-    s3cmd put "./$seed.$extension.gz" s3://comestibles.appgen.me/"$seed.$extension" --add-header=Content-Encoding:gzip
+if test -d comestibles; then
+  for geojson in $(ls comestibles|grep .geojson); do
+    seed=$(echo "$geojson"|sed 's/\.geojson\(\.gz\)$//')
+    for extension in json csv geojson; do
+      if test -f "comestibles/$seed.$extension" && ! test -f "comestibles/$seed.$extension.gz"; then
+        gzip "comestibles/$seed.$extension"
+      fi
+      s3cmd put "comestibles/$seed.$extension.gz" s3://comestibles.appgen.me/"$seed.$extension" --add-header=Content-Encoding:gzip
+    done
   done
-done
+else
+  echo I can\'t find the comestibles directory.
+fi
